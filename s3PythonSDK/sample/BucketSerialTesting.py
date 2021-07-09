0dd62a6dd8b326d02926ea58d7eb562c0dfddf0c
yuyu-rap <cat0127lovedog@gmail.com>
2021-07-09 13:20:47 +0800

    finish BuvketSerialTesting.py

--- s3PythonSDK/sample/BucketSerialTesting.py
+++ s3PythonSDK/sample/BucketSerialTesting.py
@@ -13,7 +13,7 @@ from client import client
 def main(arg):
     try:
         # putBucket with region
-        print("Try putBucket with region 'ap-northeast-1':")
+        #print("Try putBucket with region 'ap-northeast-1':")
         client.create_bucket(
             CreateBucketConfiguration={'LocationConstraint': 'ap-northeast-1'},
             Bucket=arg[2],
@@ -21,12 +21,12 @@ def main(arg):
         result = client.get_bucket_location(
             Bucket=arg[2],
         )
-        print("Bucket server location: " + repr(result['LocationConstraint']))
+        #print("Bucket server location: " + repr(result['LocationConstraint']))
         client.delete_bucket(
             Bucket=arg[2],
         )
-        print(" - Put bukcet with region done\n---------------------------------------------------")
-        print("put Bucket with 'public-read' permission..")
+        #print(" - Put bukcet with region done\n---------------------------------------------------")
+        #print("put Bucket with 'public-read' permission..")
         client.create_bucket(
             ACL='public-read',
             CreateBucketConfiguration={'LocationConstraint': 'ap-northeast-1'},
@@ -35,7 +35,7 @@ def main(arg):
         result = client.get_bucket_acl(
             Bucket=arg[0],
         )
-        print("Bucket permission: " + repr(result['Grants']))
+        #print("Bucket permission: " + repr(result['Grants']))
 
         fileName = "apple.jpg"
         fileName2 = "photos/2006/January/sample.jpg"
@@ -54,17 +54,55 @@ def main(arg):
             Key=fileName2
         )
         client.copy_object(
-                Bucket=arg[0],
-                CopySource=fileName2,
-                Key=fileName3,
-                )
+            Bucket=arg[0],
+            CopySource={'Bucket': arg[0], 'Key': fileName2},
+            Key=fileName3,
+        )
         client.put_object(
             Body='Hello World!',
             Bucket=arg[0],
             Key=fileName4
         )
 
+        #print("Get bucket normally:")
+        result = client.list_objects_v2(
+            Bucket=arg[0],
+        )
+        # for r in result['Contents']:
+        # print(r['Key'])
 
+        #print("Get bucket photos/:")
+        result = client.list_objects_v2(
+            Prefix='photos/',
+            Bucket=arg[0],
+        )
+        # for r in result['Contents']:
+        # print(r['Key'])
+
+        #print("Get bucket delimiter=/:")
+        result = client.list_objects_v2(
+            Delimiter='/',
+            Bucket=arg[0],
+        )
+        # for r in result['Contents']:
+        # print(r['Key'])
+
+        #print("Get bucket MaxKeys=2:")
+        result = client.list_objects_v2(
+            MaxKeys=2,
+            Bucket=arg[0],
+        )
+        # for r in result['Contents']:
+        # print(r['Key'])
+
+        #print("Get bucket Delimiter='/' & Prefix='photos/2006/':")
+        result = client.list_objects_v2(
+            Delimiter='/',
+            Prefix='photos/2006/',
+            Bucket=arg[0],
+        )
+        # for r in result['Contents']:
+        # print(r['Key'])
 
         print("\nClean up..")
         # clear bucket
@@ -72,7 +110,6 @@ def main(arg):
         result = client.list_objects_v2(
             Bucket=arg[0],
         )
-        print(result)
         for v in result:
             if v == 'Contents':
                 for r in result['Contents']:
