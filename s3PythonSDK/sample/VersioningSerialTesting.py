3454577b324280aadd8511c4579ddce50caab9e7
yuyu-rap <cat0127lovedog@gmail.com>
2021-07-14 12:37:35 +0800

    finish VersioningSerialTesting.py

--- s3PythonSDK/sample/VersioningSerialTesting.py
+++ s3PythonSDK/sample/VersioningSerialTesting.py
@@ -1,71 +1,126 @@
 from botocore.exceptions import ClientError
 from client import client
 
-# test 1.
-#      2.
-#      3.
+
 def main(arg):
     try:
-        bucket=conn.create_bucket(arg[0])
-        
-        #print "\nEnable Versioning :"
-        bucket.configure_versioning(True)
-        #print bucket.get_versioning_status()
-        
-        k = bucket.new_key('/photo/cht2.jpg')
-        k.set_contents_from_string('Hello',replace=True)
-        k.set_contents_from_string('Hello Python',replace=True)
-        k.set_contents_from_string('Hello Python World!',replace=True)
-        k = bucket.new_key('cht1.jpg')
-        k.set_contents_from_string('Hello',replace=True)
-        k.set_contents_from_string('Hello Python',replace=True)
-        k.set_contents_from_string('Hello Python World!',replace=True)
-        
-        result = bucket.get_all_versions()
-        
-        #print "\nList all Versions:\n  File         |   Version-ID"
-        #for v in result:
-            #print v.name + " | " + v.version_id
-        
-        result = bucket.get_all_versions(max_keys=2)
-        
-        #print "\nList Versions with max_keys=2:\n  File         |   Version-ID"
-        #for v in result:
-            #print v.name + " | " + v.version_id
-        
-        result = bucket.get_all_versions(delimiter='/')
-        
-        #print "\nList Versions with delemeter '/':\n  File         |   Version-ID"
-        #for v in result:
-            #print v.name + " | " + v.version_id
-        
-        result = bucket.get_all_versions(prefix='photo/')
-        
-        #print "\nList Versions with prefix 'photo/':\n  File         |   Version-ID"
-        #for v in result:
-            #print v.name + " | " + v.version_id
-        
-        result = bucket.get_all_versions(prefix='photo/',max_keys=2)
-        
-        #print "\nList Versions with prefix ='photo/' & max_keys=2:\n  File         |   Version-ID"
-        #for v in result:
-            #print v.name + " | " + v.version_id
-        
-        
-        #print "\nSuspend Versioning :"
-        bucket.configure_versioning(False)
-        #print bucket.get_versioning_status()
-        
-        #print "\nClean up.."
-        # clear bucket
-        result = bucket.get_all_versions()
-        for v in result:
-            bucket.delete_key(v.name,version_id=v.version_id)
-        
-        conn.delete_bucket(bucket)
-        #print " - Versioning Serial Test Done !"
-        
+        client.create_bucket(
+            CreateBucketConfiguration={'LocationConstraint': 'ap-northeast-1'},
+            Bucket=arg[0],
+        )
+        #print("\nEnable Versioning :")
+        client.put_bucket_versioning(
+            Bucket=arg[0],
+            VersioningConfiguration={
+                'MFADelete': 'Disabled',
+                'Status': 'Enabled'
+            },
+        )
+        response = client.get_bucket_versioning(
+            Bucket=arg[0]
+        )
+        #print('Status : ' + response['Status'])
+        client.put_object(
+            Bucket=arg[0],
+            Key='/photo/cht2.jpg',
+        )
+        client.put_object(
+            Body='Hello',
+            Bucket=arg[0],
+            Key='/photo/cht2.jpg',
+        )
+        client.put_object(
+            Body='Hello Python',
+            Bucket=arg[0],
+            Key='/photo/cht2.jpg',
+        )
+        client.put_object(
+            Body='Hello Python World',
+            Bucket=arg[0],
+            Key='/photo/cht2.jpg',
+        )
+        client.put_object(
+            Bucket=arg[0],
+            Key='cht1.jpg',
+        )
+        client.put_object(
+            Body='Hello',
+            Bucket=arg[0],
+            Key='cht1.jpg',
+        )
+        client.put_object(
+            Body='Hello Python',
+            Bucket=arg[0],
+            Key='cht1.jpg',
+        )
+        client.put_object(
+            Body='Hello Python World',
+            Bucket=arg[0],
+            Key='cht1.jpg',
+        )
+        result = client.list_object_versions(
+            Bucket=arg[0],
+        )
+        #print("\nList all Versions:\n  File         |   Version-ID")
+        # for v in result['Versions']:
+        #    print(v['Key'] + " | " + v['VersionId'])
+        result = client.list_object_versions(
+            Bucket=arg[0],
+            MaxKeys=2
+        )
+        #print("\nList Versions with max_keys=2:\n  File         |   Version-ID")
+        # for v in result['Versions']:
+        #    print(v['Key'] + " | " + v['VersionId'])
+        result = client.list_object_versions(
+            Bucket=arg[0],
+            Delimiter='/',
+        )
+        #print("\nList Versions with delemeter '/':\n  File         |   Version-ID")
+        # for v in result['Versions']:
+        #    print(v['Key'] + " | " + v['VersionId'])
+        result = client.list_object_versions(
+            Bucket=arg[0],
+            Prefix='/photo/',
+        )
+        #print("\nList Versions with prefix '/photo/':\n  File         |   Version-ID")
+        # for v in result['Versions']:
+        #    print(v['Key'] + " | " + v['VersionId'])
+        result = client.list_object_versions(
+            Bucket=arg[0],
+            Prefix='/photo/',
+            MaxKeys=2
+        )
+        #print("\nList Versions with prefix ='/photo/' & max_keys=2:\n  File         |   Version-ID")
+        # for v in result['Versions']:
+        #    print(v['Key'] + " | " + v['VersionId'])
+
+        #print("\nSuspend Versioning :")
+        client.put_bucket_versioning(
+            Bucket=arg[0],
+            VersioningConfiguration={
+                'MFADelete': 'Disabled',
+                'Status': 'Suspended'
+            },
+        )
+        response = client.get_bucket_versioning(
+            Bucket=arg[0]
+        )
+        #print('Status : ' + response['Status'])
+        #print("\nClean up..")
+        result = client.list_object_versions(
+            Bucket=arg[0],
+        )
+        for v in result['Versions']:
+            client.delete_object(
+                Bucket=arg[0],
+                Key=v['Key'],
+                VersionId=v['VersionId'],
+            )
+        client.delete_bucket(
+            Bucket=arg[0],
+        )
+        print("Versioning Serial Test Done !")
+
     except ClientError as e:
         print("Error operation : " + e.operation_name)
         print("Error response : " + e.response['Error']['Message'])
-    
