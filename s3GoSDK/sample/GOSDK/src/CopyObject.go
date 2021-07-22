cc303857faf161ecefadbd71e5d4ca7a29046301
yuyu-rap <cat0127lovedog@gmail.com>
2021-07-22 17:33:26 +0800

    upload copyobject

--- s3GoSDK/sample/GOSDK/src/CopyObject.go
+++ s3GoSDK/sample/GOSDK/src/CopyObject.go
@@ -5,17 +5,14 @@ import (
 	"fmt"
 
 	//"net/url"
-	"flag"
-
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func CopyObject(arg [3]string, filePath [3]string) {
-	//source := arg[0] + "/" + filePath[0]
-	destinationBucket := flag.String("d", "", arg[0]+"/"+filePath[0])
+	source := arg[0] + "/" + filePath[0]
 	input := &s3.CopyObjectInput{
 		Bucket:     &arg[1],
-		CopySource: destinationBucket,
+		CopySource: &source,
 		//aws.String(url.PathEscape(source))
 		Key: &filePath[1],
 	}
