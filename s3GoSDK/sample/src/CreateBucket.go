e1337792b6e70a69c8bdfa4ce137779510330f8d
yuyu-rap <cat0127lovedog@gmail.com>
2021-07-23 17:39:06 +0800

    upload CreatBucket.go

---
+++ s3GoSDK/sample/src/CreateBucket.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func CreateBucket(arg [3]string) {
+	for _, bucket := range arg {
+		fmt.Printf("%s\n", bucket)
+		fmt.Println("-----------------------------------------------------------------------")
+		input := &s3.CreateBucketInput{
+			ACL:    "public-read",
+			Bucket: &bucket,
+		}
+		_, create_err := Client.CreateBucket(context.TODO(), input)
+		if create_err != nil {
+			fmt.Println("Could not create bucket " + bucket)
+		}
+	}
+}
