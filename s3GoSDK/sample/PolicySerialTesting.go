d39ec3e1112e5f1e6f3bbe1a474d67145bb5a006
yuyu-rap <cat0127lovedog@gmail.com>
2021-08-05 17:50:32 +0800

    change PolicySerialTesting.go's variable name

--- s3GoSDK/sample/PolicySerialTesting.go
+++ s3GoSDK/sample/PolicySerialTesting.go
@@ -14,21 +14,21 @@ func PolicySerialTest(buckets [3]string) {
 		Policy: aws.String("{\"Version\": \"2012-10-17\", \"Statement\": [{ \"Sid\": \"DenyPublicREAD\",\"Effect\": \"Deny\",\"Principal\": {\"AWS\": \"*\"}, \"Action\": \"s3:GetObject\", \"Resource\": [\"arn:aws:s3:::" + buckets[0] + "/*\" ] } ]}"),
 	}
 
-	_, PutBucketPolicy_err := Client.PutBucketPolicy(context.TODO(), input)
-	if PutBucketPolicy_err != nil {
+	_, putbucketpolicyErr := Client.PutBucketPolicy(context.TODO(), input)
+	if putbucketpolicyErr != nil {
 		fmt.Println("Got an error PutPolicy item:")
-		fmt.Println(PutBucketPolicy_err)
+		fmt.Println(putbucketpolicyErr)
 		return
 	}
 	output := &s3.GetBucketPolicyInput{
 		Bucket: &buckets[0],
 	}
-	result, GetBucketPolicy_err := Client.GetBucketPolicy(context.TODO(), output)
-	if GetBucketPolicy_err != nil {
+	result, getbucketpolicyErr := Client.GetBucketPolicy(context.TODO(), output)
+	if getbucketpolicyErr != nil {
 		fmt.Println("Got an error GetPolicy item:")
-		fmt.Println(GetBucketPolicy_err)
+		fmt.Println(getbucketpolicyErr)
 		return
 	}
-	fmt.Println("Get Policy: \n" + *result.Policy)
+	fmt.Println("Get " + buckets[0] + " Policy: \n" + *result.Policy)
 	fmt.Println("")
 }
