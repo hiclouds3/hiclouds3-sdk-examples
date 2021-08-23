19813c83c7c9c29575437717764ea268a5958147
yuyu-rap <cat0127lovedog@gmail.com>
2021-08-23 14:04:21 +0800

    updata client.rs

---
+++ s3RustSDK/sample/src/bin/client.rs
@@ -0,0 +1,22 @@
+use s3::Credentials;
+use s3::{Client, Config, Region};
+
+pub fn client() -> Client {
+
+    let credentials = Credentials::new("","", None,None, "STATIC_CREDENTIALS");
+    
+    let region = region();
+
+    let config = Config::builder()
+        .credentials_provider(credentials)
+        .region(region)
+        .build();
+
+    Client::from_conf(config)
+}
+pub fn region() -> Region{
+    Region::new("ap-northeast-1")
+}
+fn main(){
+
+}
