958135960853dbbd8d4702cbde2be77d18462157
yuyu-rap <cat0127lovedog@gmail.com>
2021-08-30 11:48:36 +0800

    updata client.rs change region add endpoint to control hicloud

--- s3RustSDK/sample/src/client.rs
+++ s3RustSDK/sample/src/client.rs
@@ -1,15 +1,19 @@
 use s3::Credentials;
-use s3::{Client, Config, Region};
+use s3::{Client, Config, Region, Endpoint};
+use http::Uri;
 
 pub fn client() -> Client {
     let credentials = Credentials::new("","", None,None, "STATIC_CREDENTIALS");
     let region = region();
+    let uri = "http://s3.hicloud.net.tw".parse::<Uri>().unwrap();
+    let endpoint= Endpoint::mutable(uri);
     let config = Config::builder()
+        .endpoint_resolver(endpoint)
         .credentials_provider(credentials)
         .region(region)
         .build();
     Client::from_conf(config)
 }
 pub fn region() -> Region{
-    Region::new("ap-northeast-1")
+    Region::new("us-east-1")
 }
