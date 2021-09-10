8625345f14868a042db9c77b15192093c21c99bb
jeffwang0516 <jeff1jeffo@gmail.com>
2021-09-10 14:46:37 +0800

    Remove static sdk files

--- s3RubySDK/aws-sdk-ruby/examples/s3/client/get_object/03_streaming_data_to_a_block.rb
+++
@@ -1,6 +0,0 @@
-# WARNING: yielding data to a block disables retries of networking errors
-File.open('/path/to/file', 'wb') do |file|
-  s3.get_object(bucket: 'bucket-name', key: 'object-key') do |chunk|
-    file.write(chunk)
-  end
-end
