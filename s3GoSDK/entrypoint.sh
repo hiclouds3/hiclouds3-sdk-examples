125e26a7685771d7610975bb51f74fb260284149
jeffwang0516 <jeff1jeffo@gmail.com>
2021-10-06 17:19:09 +0800

    Add dockerfile for go

---
+++ s3GoSDK/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+printf 'Waiting for staas server.'
+until $(curl --output /dev/null --silent --head --fail http://s3.hicloud.net.tw); do
+    printf '.'
+    sleep 5
+done
+echo ''
+echo 'Staas Server started!'
+
+echo 'Started running go serial test...'
+cd sample/
+go mod init gosdktest
+go get
+go run *.go
+
+echo "Finished all tests... (infinite loop)"
+while true; do sleep 15 ; printf "."; done
