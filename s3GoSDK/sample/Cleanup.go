3d6a2fa880086a91d6d4e95b90415e154204a68c
jeffwang0516 <jeff1jeffo@gmail.com>
2021-11-10 13:46:49 +0800

    Prevent sample code from deleting all buckets

--- s3GoSDK/sample/Cleanup.go
+++ s3GoSDK/sample/Cleanup.go
@@ -7,45 +7,40 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func CleanUp() {
-	input1 := &s3.ListBucketsInput{}
-	result, listbucketErr := Client.ListBuckets(context.TODO(), input1)
-	if listbucketErr != nil {
-		fmt.Println("Got an error retrieving buckets:")
-		fmt.Println(listbucketErr)
-		return
-	}
-
-	for _, bucket := range result.Buckets {
+func CleanUp(buckets [3]string) {
+	for _, bucket := range buckets {
 		input2 := &s3.ListObjectsInput{
-			Bucket: bucket.Name,
+			Bucket: &bucket,
 		}
 		obj, listobjectsErr := Client.ListObjects(context.TODO(), input2)
 		if listobjectsErr != nil {
-			fmt.Println("Could not list object " + *bucket.Name)
+			fmt.Println("Could not list object " + bucket)
+			fmt.Println(listobjectsErr)
+			continue
 		}
 		for _, object := range obj.Contents {
 			output := &s3.DeleteObjectInput{
-				Bucket: bucket.Name,
+				Bucket: &bucket,
 				Key:    object.Key,
 			}
 			_, deleteobjectErr := Client.DeleteObject(context.TODO(), output)
 			if deleteobjectErr != nil {
 				fmt.Println("Got an error deleting item:")
 				fmt.Println(deleteobjectErr)
-				return
+				continue
 			}
-			fmt.Println("Deleted " + *object.Key + " from " + *bucket.Name)
+			fmt.Println("Deleted " + *object.Key + " from " + bucket)
 		}
 		input3 := &s3.DeleteBucketInput{
-			Bucket: bucket.Name,
+			Bucket: &bucket,
 		}
 		_, deleteErr := Client.DeleteBucket(context.TODO(), input3)
 		if deleteErr != nil {
-			fmt.Println("Could not delete bucket " + *bucket.Name)
-			return
+			fmt.Println("Could not delete bucket " + bucket)
+			fmt.Println(deleteErr)
+			continue
 		}
-		fmt.Println("Delete bucket : " + *bucket.Name)
+		fmt.Println("Delete bucket : " + bucket)
 	}
 	fmt.Println("")
 }
--- s3GoSDK/sample/GoSerialTest.go
+++ s3GoSDK/sample/GoSerialTest.go
@@ -7,9 +7,9 @@ import (
 func main() {
 	buckets := [3]string{"testgolangbucket1", "testgolangbucket2", "testgolangbucket3"}
 	filePath := [3]string{"castle.jpg.001", "castle.jpg.002", "castle.jpg.003"}
-	fmt.Printf("S3 GO SDK Serial Test-\nbucketname1: %s  bucketname2: %s\n", buckets[0], buckets[1])
+	fmt.Printf("S3 GO SDK Serial Test-\nbucketname1: %s  bucketname2: %s bucketname3: %s\n", buckets[0], buckets[1], buckets[2])
 	fmt.Println("-----------------------------------------------------------------------")
-	CleanUp()
+	CleanUp(buckets)
 	CreateBucket(buckets)
 	ListBucket()
 	GetBucketAcl(buckets)
